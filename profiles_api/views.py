f2b9f1931e950ae9b5da989abc04bd2cd181c6e9
Irfan Ali <75564524+irfanali1995@users.noreply.github.com>
2024-02-08 12:15:25 +0100

    Added APIView and Put,Patch,Delete method in APU

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.response import Response 
+from rest_framework import status 
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    """ Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        "Return list of APIView features"
+        an_apiview = [
+            'Uses HTTP method as function (get, post, patch, delete)',
+            'Is similar to a tradional Django View',
+            'Give you most control over app logic',
+            'Is mapped manually to URLs',
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+    
+    def post(self, request):
+        """Hello with message and name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status =status.HTTP_400_BAD_REQUEST
+                )
+    
+    def put(self, request, pk=None):
+        """Hande Updating object"""
+        return Response({'method': 'PUT'})
+    
+    def patch(self, request, pk=None):
+        """Hande partial Updating object"""
+        return Response({'method': 'PATCH'})
+    
+    def delete(self, request, pk=None):
+        """Handle deleting ing object"""
+        return Response({'method': 'DELETE'})
